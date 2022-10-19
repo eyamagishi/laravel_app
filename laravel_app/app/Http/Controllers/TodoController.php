4e788807cd634489a5dc5b2b03a58d047252f668
e.yamagishi <e.yamagishi0625@gmail.com>
2022-10-19 16:53:38 +0900

    TodoモデルをControllerにて呼び出し、インスタンス化しました

--- laravel_app/app/Http/Controllers/TodoController.php
+++ laravel_app/app/Http/Controllers/TodoController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;// 追加
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    // 追加
+    private $todo;
+
+    // 追加
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
     // 追加
     public function create()
     {
