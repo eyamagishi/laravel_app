8d1a3caba461455438aa038d1dc2f03b6bc1103e
e.yamagishi <e.yamagishi0625@gmail.com>
2022-10-24 21:29:28 +0900

    Todoモデルを論理削除に変更する

--- laravel_app/app/Todo.php
+++ laravel_app/app/Todo.php
@@ -3,12 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 追記
 
 class Todo extends Model
 {
-    // 追加
+    use SoftDeletes; // 追記
     protected $table = 'todos';
-    // 追加
     protected $fillable = [
         'content'
     ];
